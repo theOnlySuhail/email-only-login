9e71772307e5106fd7e0d2546cab2e70c9ce39e6
Suhail Kamal <Suhail.Kamal256@gmail.com>
2026-09-22 21:34:02 +0300

    chore: initialize project

---
+++ config/env.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3000),
+  GAMIL_USER: z.email(),
+  GMAIL_APP_PASS: z.string().length(16),
+});
+
+export type Env = z.infer<typeof envSchema>;
+
+function loadEnv(): Env {
+  const parsed = envSchema.safeParse(process.env);
+
+  if (!parsed.success) {
+    console.error('Invalid environment variables:');
+    console.error(parsed.error.flatten().fieldErrors);
+    process.exit(1);
+  }
+
+  return parsed.data;
+}
+
+export const env = loadEnv();
